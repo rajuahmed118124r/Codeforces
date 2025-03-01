5d50e56da1f30a42650b691c3c53e3fa14d89cfb
rajuahmed118124 <rajuahmed118124@gmail.com>
2025-03-01 16:13:48 +0600

    Initial Commit

---
+++ 2070_B_Robot_Program.cpp
@@ -0,0 +1,80 @@
+                                                            ///BISMILLAHIR RAHMANIR RAHIM
+                                                            //============================
+/*
+Problem name : B. Robot Program
+Problem link : https://codeforces.com/contest/2070/problem/B
+Platform     : Codeforces
+Author       : Md. Raju Miah
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define Raju ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define pb push_back
+#define yes cout<<'YES'<<endl
+#define no cout<<'NO'<<endl
+#define zero cout<<0<<endl
+#define all(x) (x).begin(), (x).end()
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+const int N = 1e3;
+char maze[N][N];
+int visited[N][N];
+
+void solve()
+{
+    ll n, x, k;
+    cin>>n>>x>>k;
+    string s;
+    cin>>s;
+    for(int i=0; i<n; i++)
+    {
+        if(s[i]=='L')
+        {
+            x-=1;
+        }
+        else{
+            x+=1;
+        }
+        k--;
+        if(x==0)break;
+    }
+
+    ll ans =0;
+    if(x==0)
+    {
+        ans = 1;
+        for(int i=0; i<n; i++)
+        {
+            if(s[i]=='L')
+            {
+                x-=1;
+            }
+            else{
+                x+=1;
+            }
+            if(x==0)
+            {
+                ans += k/(i+1);
+                break;
+            }
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int main()
+{
+    Raju;
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+  return 0;
+}
+                                                                //ALHAMDULILLAH
