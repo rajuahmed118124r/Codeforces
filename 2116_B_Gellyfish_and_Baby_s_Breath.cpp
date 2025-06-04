0c99d6f8b506dbd4c81a188d6097d3ddb3ba8ed1
rajuahmed118124 <rajuahmed118124@gmail.com>
2025-06-04 07:40:54 +0600

    Initial Commit

---
+++ 2116_B_Gellyfish_and_Baby_s_Breath.cpp
@@ -0,0 +1,81 @@
+                                                            ///BISMILLAHIR RAHMANIR RAHIM
+                                                            //============================
+/*
+Problem name : B. Gellyfish and Baby's Breath
+Problem link : https://codeforces.com/contest/2116/problem/B
+Platform     : Codeforces
+Author       : Md. Raju Miah
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define Raju ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define pb push_back
+#define yes cout<<'YES'<<endl
+#define no cout<<'NO'<<endl
+#define zero cout<<0<<endl
+#define all(x) (x).begin(), (x).end()
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+const int N = 1e5+5;
+const int mod = 998244353;
+vector<int> pw(N);
+
+
+void result() 
+{
+    pw[0] = 1;
+    for (int i = 1; i < N; i++) 
+    {
+        pw[i] = (2LL * pw[i - 1]) % mod;
+    }
+}
+void solve()
+{
+    int n;
+    cin>>n;
+
+    vector<int>p(n), q(n), r(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>p[i];
+    }
+    for(int i=0; i<n; i++)
+    {
+        cin>>q[i];
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        int mv = 0, vl;
+        for(int j=0; j<=i; j++)
+        {
+            vl = (pw[p[j]] + pw[q[i-j]])%mod;
+            mv = max(mv, vl); 
+        }
+        r[i]= mv;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        cout<<r[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+    Raju;
+    result();
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+  return 0;
+}
+                                                                //ALHAMDULILLAH
