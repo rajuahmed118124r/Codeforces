d939dbb6cc5e88a93f38ea0c3d44f549223e9ad9
rajuahmed118124 <rajuahmed118124@gmail.com>
2025-09-24 16:10:12 +0600

    Initial Commit

---
+++ 479_C_Exams.cpp
@@ -0,0 +1,66 @@
+                                                            // بسم الله الرحمن الرحيم
+                                                            //============================
+/*
+Problem name : C. Exams
+Problem link : https://codeforces.com/problemset/problem/479/C
+Platform     : Codeforces
+Author       : Md. Raju Miah
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define Raju ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define pb push_back
+#define yes cout<<'YES'<<endl
+#define no cout<<'NO'<<endl
+#define zero cout<<0<<endl
+#define all(x) (x).begin(), (x).end()
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+const int N = 1e3;
+char maze[N][N];
+int visited[N][N];
+
+void raju()
+{
+    int n;
+    cin>>n;
+    vector<pair<int,int>>v(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>v[i].first>>v[i].second;
+    }
+    sort(v.begin(), v.end());
+
+    int lst_day=0;
+    for(int i=0; i<n; i++)
+    {
+        int ai = v[i].first;
+        int bi = v[i].second;
+
+        if(bi>=lst_day)
+        {
+            lst_day = bi;
+        }
+        else{
+            lst_day = ai;
+        }
+    }
+    cout<<lst_day<<endl;
+}
+
+int main()
+{
+    Raju;
+    int t=1;
+    // cin>>t;
+    while(t--)
+    {
+        raju();
+    }
+  return 0;
+}
+                                                                //ٱلْحَمْدُ لِلَّٰهِ
