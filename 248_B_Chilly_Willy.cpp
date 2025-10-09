50be25c40348e3caf7984aa7e8b8f731810970a5
rajuahmed118124 <rajuahmed118124@gmail.com>
2025-10-09 15:36:38 +0600

    Initial Commit

---
+++ 248_B_Chilly_Willy.cpp
@@ -0,0 +1,63 @@
+                                                            // بسم الله الرحمن الرحيم
+                                                            //============================
+/*
+Problem name : B. Chilly Willy
+Problem link : https://codeforces.com/contest/248/problem/B
+Platform     : Codeforces
+Author       : Md. Raju Miah
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define Raju ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define pb push_back
+#define yes cout<<'YES'<<endl
+#define no cout<<'NO'<<endl
+#define zero cout<<0<<endl
+#define all(x) (x).begin(), (x).end()
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+const int N = 1e3;
+char maze[N][N];
+int visited[N][N];
+
+void raju()
+{
+    int n;
+    cin>>n;
+    vector<string>v={"050","080", "170", "020", "200", "110"};
+    if(n<=2)
+    {
+        cout<<-1<<endl;
+        return;
+    }
+    if(n==3)
+    {
+        cout<<210<<endl;
+    }
+    else{
+        cout<<"1";
+        for(int i=0; i<n-4; i++)
+        {
+            cout<<"0";
+        }
+        n-=4;
+        cout<<v[n%6]<<endl;
+    }
+}
+
+int main()
+{
+    Raju;
+    int t=1;
+    // cin>>t;
+    while(t--)
+    {
+        raju();
+    }
+  return 0;
+}
+                                                                //ٱلْحَمْدُ لِلَّٰهِ
