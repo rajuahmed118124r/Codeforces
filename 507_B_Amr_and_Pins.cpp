827ef608adea6a4f5d67ef885b55e213b72b08c1
rajuahmed118124 <rajuahmed118124@gmail.com>
2025-10-21 10:30:28 +0600

    Initial Commit

---
+++ 507_B_Amr_and_Pins.cpp
@@ -0,0 +1,48 @@
+                                                            // بسم الله الرحمن الرحيم
+                                                            //============================
+/*
+Problem name : B. Amr and Pins
+Problem link : https://codeforces.com/problemset/problem/507/B
+Platform     : Codeforces
+Author       : Md. Raju Miah
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define Raju ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define ll long long int
+#define pb push_back
+#define yes cout<<'YES'<<endl
+#define no cout<<'NO'<<endl
+#define zero cout<<0<<endl
+#define all(x) (x).begin(), (x).end()
+
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1};
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+const int N = 1e3;
+char maze[N][N];
+int visited[N][N];
+
+void raju()
+{
+    ll r, x1, y1, x2, y2, dist, ans;
+    double distance;
+    cin>>r>>x1>>y1>>x2>>y2;
+    distance = sqrt((abs(x1-x2)*abs(x1-x2)) + (abs(y1-y2)*abs(y1-y2)));
+    ans = ceil(distance/(2*r)); 
+    cout<<ans<<endl; 
+}
+
+int main()
+{
+    Raju;
+    int t=1;
+    // cin>>t;
+    while(t--)
+    {
+        raju();
+    }
+  return 0;
+}
+                                                                //ٱلْحَمْدُ لِلَّٰهِ
